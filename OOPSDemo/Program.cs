4f54683aeb9ad0caf2b20c9b4e1d277735cc6d37
sonamahapatra <sonalimahapatra105@gmail.com>
2023-04-10 18:51:13 +0530

    [sonamahapatra] Added Inheritance

---
+++ OOPSDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please Choose One From Below Option: ");
+            Console.WriteLine("1.Inheritance\n");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch(Option)
+            {
+                case 1:
+                    Car car= new Car();
+                    car.modelNumber = 2022;
+                    car.brand = "BMW";
+                    car.Start();
+                    break;
+                default:
+                    Console.WriteLine("Please Choose program with given option");
+                    break;
+            }
+                
+
+            
+            Console.ReadLine();
+        }
+    }
+}
