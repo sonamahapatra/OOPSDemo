dbaada52536306fbf3968723474b536476c12fd8
sonamahapatra <sonalimahapatra105@gmail.com>
2023-04-10 19:11:26 +0530

    [sonamahapatra] Adding Method OverLoading

--- OOPSDemo/Program.cs
+++ OOPSDemo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OOPSDemo.polymorphism;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.ConstrainedExecution;
@@ -12,7 +13,7 @@ namespace OOPSDemo
         public static void Main(string[] args)
         {
             Console.WriteLine("Please Choose One From Below Option: ");
-            Console.WriteLine("1.Inheritance\n");
+            Console.WriteLine("1.Inheritance\n2.polymorphism\n");
             int Option = Convert.ToInt32(Console.ReadLine());
             switch(Option)
             {
@@ -21,6 +22,10 @@ namespace OOPSDemo
                     car.modelNumber = 2022;
                     car.brand = "BMW";
                     car.Start();
+                    break;
+                    case 2:
+                        Addition addition = new Addition();
+                    addition.AddNumbers(10, 45.67, 50);
                     break;
                 default:
                     Console.WriteLine("Please Choose program with given option");
---
+++ OOPSDemo/polymorphism/MethodOverLoading.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSDemo.polymorphism
+{
+    internal class Addition
+    {
+        public void AddNumbers(int first , int second)
+        {
+            Console.WriteLine("Sum:" + first + second);
+        }
+        //varying number of parameters compared to 1st method
+        public void AddNumbers(int first ,int second , int third)
+        {
+           double Result = first + second + third; 
+            Console.WriteLine("Sum:" + Result);
+        }
+        //Changing datatype compared to 2nd method
+        public void AddNumbers(int first ,double second, int third)
+        {
+            double Result = first + second + third;
+            Console.WriteLine("Sum:" + Result);
+        }
+        //Changing order compared to 3rd method
+        public void AddNumbers(double second , int first,int third)
+        {
+            double Result = first + second + third;
+            Console.WriteLine("Sum:" + Result);
+        }
+    }
+}
